8f4b9af39a3335bb3d50104a4889ff04aeec3581
Rob LaRubbio <rob@pugdogdev.com>
2010-08-02 10:13:36 -0700

    Clean up the code a little and compress the json a little

--- wsll/rest/views.py
+++ wsll/rest/views.py
@@ -10,40 +10,47 @@ from django.utils import simplejson
 
 def spirits(request):
     spirits = []
+    rows = []
+    data = []
 
     if 'id' in request.GET:
         spirits = get_list_or_404(Spirit, pk=request.GET['id'])
     elif 'name' in request.GET:
         spirits = get_list_or_404(Spirit, brand_name=request.GET['name'])
-    elif 'search' in request.GET:
-        spirits = Spirit.objects.all().filter(brand_name__icontains=request.GET['search'])
     elif 'category' in request.GET:
         spirits = get_list_or_404(Spirit, category=request.GET['category'])
+    elif 'search' in request.GET:
+        cursor = connection.cursor()
+
+        cursor.execute("select brand_name, count(*), id from spirits where brand_name like '%%" + request.GET['search'] + "%%' group by brand_name order by brand_name")
+        
+        rows = cursor.fetchall()
     else:
         cursor = connection.cursor()
 
         cursor.execute("select brand_name, count(*), id from spirits group by brand_name order by brand_name")
         
-        data = []
         rows = cursor.fetchall()
+
+    # If spirits has data I want the fuller json dict
+    if len(spirits):
+        for s in spirits:
+            sd = s.dict()
+            data.append({'brand_name': sd['brand_name'], 
+                         'id': sd['id'],
+                         'size': sd['size'],
+                         'price': sd['total_retail_price']})
+
+    # If rows has data I need the more compressed json dict
+    if len(rows):
         for row in rows:
-            info = {'brand_name': row[0], 'count': row[1]}
+            info = {'n': row[0]}
             if row[1] == 1:
                 info['id'] = row[2]
+            else:
+                info['c'] = row[1]
 
             data.append(info)
-
-        mimetype = 'application/json'
-        return HttpResponse(simplejson.dumps(data),mimetype)
-
-
-    data = []
-    for s in spirits:
-        sd = s.dict()
-        data.append({'brand_name': sd['brand_name'], 
-                     'id': sd['id'],
-                     'size': sd['size'],
-                     'total_retail_price': sd['total_retail_price']})
 
     mimetype = 'application/json'
     return HttpResponse(simplejson.dumps(data),mimetype)
